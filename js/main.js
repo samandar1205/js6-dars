283725cafe20027ab76ee49677c4cd1f53c88cb3
samandar12 <samandarkomiljonov1212@gmail.com>
2023-05-21 19:34:14 +0500

    js 6-dras
    bu js 6-dras kodlari

--- js/main.js
+++ js/main.js
@@ -1,49 +1,50 @@
-/*let round = Math.round(8,5);
-let pow = Math.pow(4,4);
-let sqrt = Math.sqrt(81);
-let abs = Math.abs(-12.8);
-let ceil = Math.ceil(4.5);
-let floor = Math.floor(8.9);
-let min = Math.min(12,1,31,18,12847,54187);
-let max = Math.max(12,1,31,18,12847,54187);
-let trunc = Math.trunc(4.5)
-document.write(trunc)*/
+/* Math in JS */
+/*let round = Math.round(8.6); /!* yaxlitlash *!/
+let pow = Math.pow(4,4); /!* darajaga topish *!/
+let sqrt = Math.sqrt(81); /!* ildizini topish *!/
+let abs = Math.abs(-12.8); /!* absolutlash *!/
+let ceil = Math.ceil(8.1); /!* yaxlitlash *!/
+let floor = Math.floor(8.9) /!* yaxlitlash *!/
+let min = Math.min(12,1,18,95,2004,15,18); /!* minimalini topish *!/
+let max = Math.max(12,1,18,95,2004,4958,4959); /!* maximalini topish *!/
+let trunc = Math.trunc(4.5); /!*  *!/
+document.write(trunc);*/
 
-/*localStorage.setItem('ism','Olov');
-localStorage.setItem('familiya','sayfullayev');
-
+/* localStorage(mahalliy ombor) in JS */
+/*localStorage.setItem('ism','Olovlov');
+localStorage.setItem('familiya','Sayfulloyev');
+localStorage.setItem('yoshi','14');
 localStorage['ism'] = "Olovuddin";
-localStorage['Familiyasi'] = "sayfullayev"*/
-
-/*localStorage.removeItem('ism')
-localStorage.removeItem('familiya')*/
-
-/*localStorage.clear()
-
+localStorage['familiya'] = "Sayfullayev";
+/!*localStorage.removeItem('ism');
+localStorage.removeItem('familiya');*!/
+localStorage.clear();
 let uningIsmi = localStorage.getItem('ism');
-let uningFamiliyasi = localStorage.getItem('familiyasi');*/
+let uningFamiliyasi = localStorage.getItem('familiya');
+let uningYoshi = localStorage.getItem('yoshi');
+console.log(uningIsmi,uningFamiliyasi,uningYoshi);*/
 
-/*console.log(uningIsmi,uningFamiliyasi);*/
-
-/*
-let sorov = new XMLHttpRequest();
-sorov.open('get','https://getty.uz/serverdan/malumot/olish');   /!* get , post *!/
-*/
-
-/*let object = {ism:"Olov",yoshi:14};
-
-let json = '{"ism":"Olov","yoshi":13}';
-
-/!*console.log(JSON.parse(json));*!/
-
-console.log(JSON.stringify(object));*/
-
-let sorov = new XMLHttpRequest();
-sorov.open('get' ,'jsonFayl.json')
+/* https requests in JS */
+/*let sorov = new XMLHttpRequest();
+sorov.open('get','https://getty.uz/serverdan/malumot/olish');
 sorov.send();
 sorov.onload = function (){
-    console.log(JSON.stringify(sorov));
+    console.log(sorov.responseText);
+}*/
+
+/* JSON in JS */
+/*let object = {ism: "Oluv",yoshi: 13};   /!*  => JS object file*!/
+let json = '{"ism":"Oluv","yoshi":13}'; /!*  => Json file*!/
+console.log(JSON.parse(json));*/
+
+let sorov = new XMLHttpRequest();
+sorov.open('get','jsonFayl.json');
+sorov.send();
+sorov.onload = function (){
+    console.log(sorov.responseText);
 }
+
+
 
 
 
